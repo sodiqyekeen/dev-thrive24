177a93daf63429fc165e5adbc7974607310f809b
godsAmbassador <adelekesamuel080@gmail.com>
2023-12-13 14:16:37 +0100

    transaction table

--- skillbridge/entities.sql
+++ skillbridge/entities.sql
@@ -1,37 +1,14 @@
 
-DROP TABLE IF EXISTS faculty;
-DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS transact;
 
 PRAGMA foreign_keys = ON;
 
--- Faculty table
-CREATE TABLE faculty (
+
+-- Transaction table
+CREATE TABLE transact(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    faculty_name VARCHAR(255) UNIQUE NOT NULL,
+    payment_reference VARCHAR(225),
+    status_payment VARCHAR(10) UNIQUE NOT NULL,
+    progress_level VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Department table
-CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    department_name VARCHAR(255) UNIQUE NOT NULL,
-    faculty_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (faculty_id) REFERENCES faculty(id) ON DELETE CASCADE
-);
-
--- Users table
-CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    matric_no VARCHAR(255) UNIQUE NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    department_id INT NOT NULL,
-    level INT NOT NULL,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
--- skillbridge/seed-data.sql
+++ skillbridge/seed-data.sql
@@ -1,18 +1,5 @@
--- Seed data for faculty table
--- Seed data for faculty table
+-- Seed data for transact table
 
-INSERT INTO faculty (faculty_name) VALUES ('Computing and Information Technology');
-INSERT INTO faculty (faculty_name) VALUES ('Engineering');
-INSERT INTO faculty (faculty_name) VALUES ('Health and Life Sciences');
-INSERT INTO faculty (faculty_name) VALUES ('Humanities and Social Sciences');
 
--- Seed data for department table
-
-INSERT INTO department (department_name, faculty_id) VALUES ('Computer Science', 1);
-INSERT INTO department (department_name, faculty_id) VALUES ('Information Technology', 1);
-INSERT INTO department (department_name, faculty_id) VALUES ('Software Engineering', 2);
-INSERT INTO department (department_name, faculty_id) VALUES ('Electrical Engineering', 2);
-INSERT INTO department (department_name, faculty_id) VALUES ('Mechanical Engineering', 2);
-INSERT INTO department (department_name, faculty_id) VALUES ('Nursing', 3);
-INSERT INTO department (department_name, faculty_id) VALUES ('Medicine', 3);
-
+INSERT INTO transact (payment_reference, status_payment, progress_level) VALUES ('qqw13dsvdfge24ccbfgj', 'paid', 'completed');
+INSERT INTO transact ( status_payment, progress_level) VALUES ( 'free', 'started');
