1b597afa8a54ab524f4197e562fe72e28bcb9e85
Boluwatife Oke <Okeboluwatife8@gmail.com>
2023-12-14 12:21:50 +0100

    skill table

---
+++ seed-sql.sql
@@ -0,0 +1,26 @@
+-- Seed data for faculty table
+-- Seed data for faculty table
+
+-- INSERT INTO faculty (faculty_name) VALUES ('Computing and Information Technology');
+-- INSERT INTO faculty (faculty_name) VALUES ('Engineering');
+-- INSERT INTO faculty (faculty_name) VALUES ('Health and Life Sciences');
+-- INSERT INTO faculty (faculty_name) VALUES ('Humanities and Social Sciences');
+
+-- Seed data for department table
+
+-- INSERT INTO department (department_name, faculty_id) VALUES ('Computer Science', 1);
+-- INSERT INTO department (department_name, faculty_id) VALUES ('Information Technology', 1);
+-- INSERT INTO department (department_name, faculty_id) VALUES ('Software Engineering', 2);
+-- INSERT INTO department (department_name, faculty_id) VALUES ('Electrical Engineering', 2);
+-- INSERT INTO department (department_name, faculty_id) VALUES ('Mechanical Engineering', 2);
+-- INSERT INTO department (department_name, faculty_id) VALUES ('Nursing', 3);
+-- INSERT INTO department (department_name, faculty_id) VALUES ('Medicine', 3);
+
+-- seed data for skills table
+
+INSERT INTO skill (skill_name, description) VALUES ("Data Science", "lorem ipsum lorem ipsum");
+INSERT INTO skill (skill_name, description) VALUES ("Website Design", "lorem ipsum lorem ipsum");
+INSERT INTO skill (skill_name, description) VALUES ("App development", "lorem ipsum lorem ipsum");
+INSERT INTO skill (skill_name, description) VALUES ("Network Security", "lorem ipsum lorem ipsum");
+INSERT INTO skill (skill_name, description) VALUES ("Machine learning", "lorem ipsum lorem ipsum");
+INSERT INTO skill (skill_name, description) VALUES ("Graphics Design", "lorem ipsum lorem ipsum");
