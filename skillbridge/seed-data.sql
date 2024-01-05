39e621f9a86e0a0d59ed20312507e8cbcb97d7ef
godsAmbassador <adelekesamuel080@gmail.com>
2024-01-05 21:44:45 +0100

    transact table

--- skillbridge/seed-data.sql
+++ skillbridge/seed-data.sql
@@ -1,5 +1,5 @@
 -- Seed data for transact table
 
 
-INSERT INTO transact (payment_reference, status_payment, progress_level) VALUES ('qqw13dsvdfge24ccbfgj', 'paid', 'completed');
-INSERT INTO transact ( status_payment, progress_level) VALUES ( 'free', 'started');
+-- INSERT INTO transact (payment_reference, status_payment, progress_level) VALUES ('qqw13dsvdfge24ccbfgj', 'paid', 'completed');
+-- INSERT INTO transact ( status_payment, progress_level) VALUES ( 'free', 'started');
