18b756550f193748a311b8c9bc03fa1059ef5cec
Tonia2444 <feyisayoakinsanmi48@gmail.com>
2024-01-11 14:17:43 +0100

    adds my_utilities and update rating&dept. service

--- skillbridge/dep_services/department_service.py
+++ skillbridge/dep_services/department_service.py
@@ -19,20 +19,22 @@ def get_department_by_id(department_id):
     department_data = cur.fetchone()
     if department_data is None:
         return None
-    department = Department(department_data['id'], department_data['name'])
+    department = Department(department_data['id'], department_data['name'], department_data['faculty_id'])
     return department
 
-def add_new_department(department_name):
+def add_new_department(department_name, faculty_id):
     db = get_db()
     cur = db.cursor()
     cur.execute("INSERT INTO department (name) VALUES (?)", (department_name,))
+    cur.execute("SELECT * FROM department WHERE faculty_id = ?", (faculty_id))
     db.commit()
     return cur.lastrowid
 
-def department_exists(department_name):
+def department_exists(department_name, faculty_id):
     db = get_db()
     cur = db.cursor()
     cur.execute("SELECT * FROM department WHERE name = ?", (department_name,))
+    cur.execute("SELECT * FROM department WHERE faculty_id = ?", (faculty_id))
     department_data = cur.fetchone()
     if department_data is None:
         return False
