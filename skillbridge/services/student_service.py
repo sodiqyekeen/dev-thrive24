0829bb62b915b4056202a662a35eaa1e5d49c4ce
GodsAMBASSADOR <135479874+GodsAMBASSADOR@users.noreply.github.com>
2024-01-16 12:20:27 +0100

    Update student_service.py

--- skillbridge/services/student_service.py
+++ skillbridge/services/student_service.py
@@ -1,20 +1,6 @@
 from skillbridge.db import *
 from skillbridge.models.student_model import *
 
-""""
-CREATE TABLE student (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    matric_no VARCHAR(255) UNIQUE NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    department_id INT NOT NULL,
-    level INT NOT NULL,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-"""
 def get_all_students():
     db = get_db()
     cur = db.cursor()
