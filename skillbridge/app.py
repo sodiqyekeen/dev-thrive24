63193324df8a423e801baa17603ecf9a3e2ad971
godsAmbassador <adelekesamuel080@gmail.com>
2024-02-02 01:46:18 +0100

    added transaction blueprint

--- skillbridge/app.py
+++ skillbridge/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 import db
-from routes import facultyroute, department_route, project_route, ratingroute, studentroute, notification_route,user_role_route
+from routes import facultyroute, department_route, project_route, ratingroute, studentroute, notification_route,user_role_route, transact_route
 from flasgger import Swagger
 
 
@@ -61,5 +61,6 @@ app.register_blueprint(ratingroute.rating_endpoint, url_prefix='/api/ratings')
 app.register_blueprint(studentroute.student_endpoint, url_prefix='/api/students')
 app.register_blueprint(notification_route.notification_service_endpoint, url_prefix='/api/notification')
 app.register_blueprint(user_role_route.user_role_endpoint, url_prefix='/api/user_role')
+app.register_blueprint(transact_route.transaction_endpoint, url_prefix='/api/transaction')
 if __name__ == "__main__":
     app.run()
